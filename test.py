d3f8579eb12da532de1528034999f909fc4e4ccd
wski <matt@lootsafe.io>
2018-05-08 04:54:05 -0600

    changed a few things, added comments here and there, this commit message sucks

--- test.py
+++ test.py
@@ -2,13 +2,13 @@ from hashtree import HashTree
 import numpy as np
 
 
-data = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i']
+data = ['a', 'b', 'c', 'd']
 tree = HashTree(data)
 tree.hash()
 
 print "Merkle Root: " + tree.root
 print "Transactions:"
-for hash in tree.hashes:
+for hash in tree.transactions:
     print hash
 print "Tree:"
 print(np.matrix(tree.tree))
